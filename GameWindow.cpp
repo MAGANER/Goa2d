70efc80ba9cefa5b67e3fa73ae33f38520498b03
MAGANER <maganer.krik@mail.ru>
2023-08-16 18:01:41 +0300

    added special struct to represent data that can be passed to constructor. But constructor can get too many arguments, so i deicided to move them to separate structure

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -1,32 +1,39 @@
 #include"GameWindow.h"
 using namespace Goat2d::core;
 
-GameWindow::GameWindow(int width, int height, const std::string& title)
+GameWindow::GameWindow(const GameWindowSetting& setting)
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	print_error = setting._print_error;
+	write_error = setting._write_error;
+
+	if (SDL_Init(setting.SDL_subsystems) < 0)
 	{
-		//TODO::write debug info into log file
-		std::cout << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+		if(print_error)::print_error("SDL could not initialize! SDL_Error:");
+		if (write_error)::write_error("SDL could not initialize! SDL_Error:");
 	}
 	else
 	{
 		//Create window
-		window = SDL_CreateWindow(title.c_str(),
-								  SDL_WINDOWPOS_UNDEFINED, 
-								  SDL_WINDOWPOS_UNDEFINED, 
-								  width, 
-								  height, 
+		window = SDL_CreateWindow(setting.title.c_str(),
+								  setting.win_pos.x, 
+								  setting.win_pos.y,
+								  setting.win_size.x,
+								  setting.win_size.y,
 								  SDL_WINDOW_SHOWN);
 		if (window == NULL)
 		{
-			std::cout << "Window could not be created! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+			if(print_error)::print_error("failed to create window: SDL_Error:");
+			if (write_error)::write_error("failed to create window: SDL_Error:");
 		}
 
 		//Get window surface
 		screenSurface = SDL_GetWindowSurface(window);
 
 		//Fill the surface black
-		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0));
+		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 
+												     setting.background_color.r,
+													 setting.background_color.g,
+													 setting.background_color.b));
 
 		//Update the surface
 		SDL_UpdateWindowSurface(window);
@@ -47,10 +54,11 @@ void GameWindow::run()
 {
 	SDL_Event e;
 	bool quit = false;
-	while (quit == false)
+	while (!quit)
 	{
 		while (SDL_PollEvent(&e))
 		{
+			//default event
 			if (e.type == SDL_QUIT)
 				quit = true;
 		}
--- GameWindow.h
+++ GameWindow.h
@@ -1,12 +1,74 @@
 #ifndef GAME_WINDOW_H
 #define GAME_WINDOW_H
-#include"SDL.h"
-#include<string>
-#include<iostream>
+#include"ErrorLogger.h"
+#include"Color.h"
+#include"Vector.h"
 namespace Goat2d
 {
 namespace core
 {
+	/*
+	* GameWindowSetting is structure that contains parameters,
+	* that should be provided to GameWindow class to set its initial state
+	*/
+	struct GameWindowSetting
+	{
+		int SDL_subsystems; //SDL_INIT_VIDEO or SDL_INIT_VIDEO | SDL_INIT_AUDIO e.t.c
+		std::string title;
+		Color background_color;
+		bool _print_error, _write_error;
+		Vector2i win_pos, win_size;
+
+		GameWindowSetting()
+		{
+			SDL_subsystems = SDL_INIT_VIDEO;
+			win_size = Vector2i(720, 640);
+			title = "Goat2d";
+			background_color = Color(0, 0, 0, 255);
+			_print_error = true;
+			_write_error = true;
+			win_pos = Vector2i(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+		}
+		GameWindowSetting(const GameWindowSetting& setting)
+		{
+			SDL_subsystems = setting.SDL_subsystems;
+			win_size = setting.win_size;
+			title = setting.title;
+			background_color = setting.background_color;
+			_print_error = setting._print_error;
+			_write_error = setting._write_error;
+			win_pos = setting.win_pos;
+		}
+		GameWindowSetting(int SDL_subsystems,
+						  const Vector2i& win_size)
+		{
+			this->SDL_subsystems = SDL_subsystems;
+			this->win_size = win_size;
+			std::cout << win_size.x << " " << win_size.y << std::endl;
+			background_color = Color(0, 0, 0, 255);
+			_print_error = true;
+			_write_error = true;
+			win_pos = Vector2i(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+		}
+		GameWindowSetting(int SDL_subsystems,
+						  const Vector2i& win_size,
+						  const Vector2i& win_pos,
+						  const std::string& title,
+						  const Color& background_color,
+						  bool _print_error,
+						  bool _write_error)
+		{
+			this->SDL_subsystems = SDL_subsystems;
+			this->win_size = win_size;
+			this->background_color = background_color;
+			this->_print_error = _print_error;
+			this->_write_error = _write_error;
+			this->win_pos = win_pos;
+		}
+		~GameWindowSetting(){}
+
+	};
+
 	class GameWindow
 	{
 		//The window we'll be rendering to
@@ -16,8 +78,9 @@ namespace core
 		SDL_Surface* screenSurface = nullptr;
 
 		bool ok = false;
+		bool print_error, write_error;
 	public:
-		GameWindow(int width, int height, const std::string& title);
+		GameWindow(const GameWindowSetting& setting);
 		~GameWindow();
 
 		bool is_ok() const { return ok; }
@@ -26,6 +89,4 @@ namespace core
 	};
 };
 };
-
-
 #endif //GAME_WINDOW_H
