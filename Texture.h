ef46141ad71fc5bbb2721af56bb0b71759b1c0ca
MAGANER <maganer.krik@mail.ru>
2023-08-21 12:07:55 +0300

    added additional comments

--- Texture.h
+++ Texture.h
@@ -6,12 +6,16 @@ namespace Goat2d
 {
 namespace core 
 {
+	/*
+	 This class is used to load graphical images
+	 and use them in game.
+	*/
 	class Texture: public DrawableObject
 	{
-		SDL_Texture* texture = nullptr;
-		SDL_Rect* drawing_rect = nullptr;
+		SDL_Texture* texture = nullptr;//pixel data
+		SDL_Rect* drawing_rect = nullptr;//size, pos
 
-		bool ok = true;
+		bool ok = true; //if it's true, than everything loaded correctly
 	public:
 		Texture(const std::string& path,SDL_Renderer* renderer);
 		~Texture();
