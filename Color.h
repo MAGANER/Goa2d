b73e169764b3a8b2b0b123242a17414ac6a411cf
MAGANER <maganer.krik@mail.ru>
2023-08-21 11:58:08 +0300

    erased unused function

--- Color.h
+++ Color.h
@@ -7,10 +7,6 @@ namespace core
 {
 	typedef SDL_Color Color;
 
-	static inline Color make_color(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
-	{
-		return Color{.r=r,.g=g,.b=b,.a=a};
-	}
 	static inline void copy_color(Color* dest, Color* source)
 	{
 		//TODO::add checks if those pointers aren't nulls
