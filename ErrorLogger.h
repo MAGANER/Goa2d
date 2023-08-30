9d1d52eb9962453a78d951ab92fa3459e4d806bb
MAGANER <maganer.krik@mail.ru>
2023-08-30 23:51:35 +0300

    fixed file writing. Create error_log.txt and append to the end time and message

--- ErrorLogger.h
+++ ErrorLogger.h
@@ -9,17 +9,16 @@ namespace Goat2d
 namespace core 
 {
 	//these proceduras are used to print/write special SDL Errors
-	static void print_error(const std::string& additional_info)
+	static inline void print_error(const std::string& additional_info)
 	{
 		std::cout << additional_info << SDL_GetError()<<std::endl;
 	}
 	static void write_error(const std::string& additional_info)
 	{
 		std::time_t t = std::time(0);
-		auto log_file_name = std::string(std::ctime(&t));
-
-		std::ofstream f(log_file_name);
-		f << additional_info << SDL_GetError()<<std::endl;
+		auto time = std::string(std::ctime(&t));
+		std::ofstream f("error_log.txt",std::ios::app);
+		f <<time<<" : "<< additional_info << SDL_GetError() << std::endl;
 	}
 	//TODO:: write same proceduras for Goat2d errors
 };
