f51e5c9999987a18ec69bfa46f48c828afed25b6
MAGANER <maganer.krik@mail.ru>
2023-09-17 12:01:43 +0300

    added simple timer class to compute time differences

---
+++ core/Timer.cpp
@@ -0,0 +1,16 @@
+#include"Timer.h"
+using namespace Goat2d::core;
+
+void Timer::start()
+{
+	start_point = std::chrono::steady_clock::now();
+}
+void Timer::stop()
+{
+	end_point = std::chrono::steady_clock::now();
+}
+double Timer::get_elapsed_ticks()
+{
+	const std::chrono::duration<double> diff = end_point - start_point;
+	return diff.count();
+}
---
+++ core/Timer.h
@@ -0,0 +1,52 @@
+/*!
+\file
+\brief Class to compute elapsed time
+\author Maganer
+\version 0.2
+\date 02.09.2023
+*/
+
+
+#ifndef TIMER_H
+#define TIMER_H
+#include<chrono>
+namespace Goat2d
+{
+namespace core
+{
+
+
+	/*!
+	\brief Class to compute elapsed time
+	\author MAGANER
+	\date 10.09.2023
+	*/
+	class Timer
+	{
+		//! time point of computation start
+		std::chrono::time_point<std::chrono::steady_clock> start_point;
+
+		//! time point of computation finish
+		std::chrono::time_point<std::chrono::steady_clock> end_point;
+	public:
+		//! creates timer
+		Timer() {}
+
+
+		//! delete timer
+		~Timer() {}
+
+
+		//!  set time point of computation start.
+		void start();
+
+
+		//! set time point of computation finish.
+		void stop();
+
+		//! compute the difference between end point and start point.
+		double get_elapsed_ticks();
+	};
+};
+};
+#endif //TIMER_H
